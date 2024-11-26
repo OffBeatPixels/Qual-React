26d9e48de49f2d1a28313a126301e302adcbc3d4
OffBeatPixels <offbeatpixels@gmail.com>
2024-11-26 16:42:37 +0530

    Remove all files from gh-pages branch

--- babel.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: [
-    "@babel/preset-env", // Transpile modern JavaScript
-    ['@babel/preset-react', {// Transpile JSX
-      runtime: 'automatic', // Use automatic runtime for React 17+
-    }], 
-  ],
-  plugins: ["@babel/plugin-transform-modules-commonjs"],
-};
